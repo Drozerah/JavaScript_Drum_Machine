76f2db0d855f2bc6a100349c6aecf25cf69c1099
Drozerah <drozerah@drozerah.user>
2019-03-20 21:06:56 +0100

    feat: add module pattern

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
 import './styles/main.scss' 
 console.log('I am the endry point!'); 
+
+const drumMachine = (()=>{
+    let self = {}
+    self.init = () => {
+ 
+        let drumPads = Array.from(document.querySelectorAll('.drum-pad'))
+        console.log(typeof drumPads);
+        console.log(drumPads);
+
+    }
+    return self
+})()
+
+drumMachine.init()
